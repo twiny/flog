b4722f18ba3e58f0eca6a0a92ecedc91410205fd
MI <hello@superiss.com>
2022-03-20 19:50:01 +0100

    update test cases

--- logger_test.go
+++ logger_test.go
@@ -5,8 +5,80 @@ import (
 	"testing"
 )
 
-func TestLogWrite(t *testing.T) {
-	// todo: implement
+func TestLogInfoWrite(t *testing.T) {
+	t.Parallel()
+
+	logger, err := NewLogger("logs", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logger.Close()
+
+	cases := []struct {
+		name    string
+		message string
+	}{
+		{
+			name:    "test_info",
+			message: "hello univers :)",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			logger.Info(c.message, map[string]string{})
+		})
+	}
+}
+func TestLogErrorWrite(t *testing.T) {
+	t.Parallel()
+
+	logger, err := NewLogger("logs", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logger.Close()
+
+	cases := []struct {
+		name    string
+		message string
+	}{
+		{
+			name:    "test_error",
+			message: "something went wrong :(",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			logger.Error(c.message, map[string]string{})
+		})
+	}
+}
+func TestLogFetalWrite(t *testing.T) {
+	t.Parallel()
+
+	logger, err := NewLogger("logs", "test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer logger.Close()
+
+	cases := []struct {
+		name    string
+		message string
+	}{
+		{
+			name:    "test_fetal",
+			message: "something seriously went wrong :O",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run("", func(t *testing.T) {
+			logger.Fetal(c.message, map[string]string{})
+		})
+	}
 }
 
 func BenchmarkLogWrite(b *testing.B) {
@@ -21,17 +93,15 @@ func BenchmarkLogWrite(b *testing.B) {
 			"number": strconv.Itoa(i),
 		})
 	}
-
 }
 func BenchmarkParallelLogWrite(b *testing.B) {
 	logger, err := NewLogger("logs", "test")
 	if err != nil {
 		b.Fatal(err)
 	}
+	defer logger.Close()
 
 	b.RunParallel(func(p *testing.PB) {
-		defer logger.Close()
-
 		n := 0
 		for p.Next() {
 			logger.Info("benchmark", map[string]string{
